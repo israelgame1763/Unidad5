55cde1fb20ead12a025896fa1e31f513be1cbf2c
israelgame1763 <108910870+israelgame1763@users.noreply.github.com>
2023-11-10 23:26:54 -0600

    Add files via upload

---
+++ ArreglosYmetodos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arreglosymetodos;
+
+/**
+ *
+ * @author aguil
+ */
+public class ArreglosYmetodos {
+
+    private static String prom;
+
+    public static void main(String[] args) {
+        double[][] calificaciones = {
+                {90.5, 85.0, 78.5, 92.0},
+                {88.0, 76.5, 89.0, 94.5},
+                {70.0, 82.5, 91.0, 87.5}
+        };
+
+
+        System.out.println("Promedio por estudiante: " + prom
+
+    }
+}
